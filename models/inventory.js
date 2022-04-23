4421c60084e578da5dd541b331cae53546805cee
wnrakvp <wnr.akvp@gmail.com>
2022-04-23 16:23:38 +0700

    RemoveInventoryFromStock

--- models/inventory.js
+++ models/inventory.js
@@ -1,30 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const InventorySchema = new mongoose.Schema({
-    
-    date:{
-        type: Date,
-        required: true
-    },
-    warehousetype: {
-        type: String,
-        required: [true, 'Please add Warehouse'],
-    },
-    availableamount: {
-        type: Number,
-        required: [true, 'Please add amount']
-    },
-    // product:{
-    //     type: mongoose.Schema.ObjectId,
-    //     ref: 'Product',
-    //     required: true
-    // },
-    sku:{
-        type: mongoose.Schema.ObjectId,
-        ref: 'Sku',
-        required: true
-    }
+  date: {
+    type: Date,
+    required: true,
+  },
+  warehousetype: {
+    type: String,
+    required: [true, "Please add Warehouse"],
+  },
+  availableamount: {
+    type: Number,
+    required: [true, "Please add amount"],
+  },
+  // product:{
+  //     type: mongoose.Schema.ObjectId,
+  //     ref: 'Product',
+  //     required: true
+  // },
+  sku: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Sku",
+    required: true,
+  },
 });
 
+InventorySchema.pre("remove", async function (next) {
+  console.log(`Stocks being removed from Inventory ${this._id}`);
+  await this.model("Stock").updateMany(
+    {},
+    {
+      '$pull': {
+        'items': {
+          'inventory': { _id : this._id},
+        },
+      },
+    }
+  );
+  next();
+});
 //objectId product sku
 
-module.exports = mongoose.model('Inventory', InventorySchema);
+module.exports = mongoose.model("Inventory", InventorySchema);
